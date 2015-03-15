fd9ce39e0221aba5349575ef21ce6d4f2780c427
Tiffany <thlam@berkeley.edu>
2015-03-15 16:21:13 -0700

    added bargraph

--- dynamic3.js
+++ dynamic3.js
@@ -42,6 +42,17 @@ dynamic3.Graph.prototype = {
         this.options.width = width;
         return this;
     },
+    
+    setDomain: function(domain) {
+        this.options.domain = domain;
+        return this;
+    },
+    
+    __updateStyles: function(chart) {
+        chart.style('fill', this.options.backgroundColor);
+        chart.style('stroke', this.options.borderColor);
+        chart.style('stroke-width', this.options.borderWidth);
+    },
 
     finishSetup: function(node) {
         this.ctx = d3.select(node)
@@ -74,7 +85,7 @@ dynamic3.CircleGraph.prototype = {
                         .data([data]);
 
         var width = this.options.width ;
-        var r = data > width ? width : data;
+        var r = d3.scale.linear().domain(this.options.domain || [0, 1]).range([5, this.options.width / 2]);
         var cx = this.options.width / 2;
         var cy = this.options.height / 2;
 
@@ -82,6 +93,8 @@ dynamic3.CircleGraph.prototype = {
              .attr('r', r)
              .attr('cx', cx)
              .attr('cy', cy)
+        
+        this.__updateStyles(chart)
 
         chart.transition()
              .duration(this.options.transitionTime)
@@ -89,12 +102,60 @@ dynamic3.CircleGraph.prototype = {
              .attr('cx', cx)
              .attr('cy', cy)
 
-        chart.exit().remove()
+        //chart.exit().remove()
     }
 };
 
+dynamic3.BarGraph = function() { 
+    dynamic3.Graph.call(this);
+}
+
+dynamic3.BarGraph.prototype = {
+    __proto__: dynamic3.Graph.prototype,
+    
+    setPadding: function(padding) {
+        this.options.padding = padding;
+        return this;
+    },
+    
+    update: function(data) {
+        var oldData = this.data;
+        if (oldData) {
+            console.assert(oldData.length == data.length)
+        }
+        this.data = data;
+        
+        var width = d3.scale.linear().domain(this.options.domain || [0, 1]).range([0, this.options.width]);
+        var padding = this.options.padding || 2;
+        var height = this.options.height / data.length - padding;
+
+        var chart = this.ctx.selectAll('rect')
+                        .data(data);
+    
+                        chart.enter().append('svg:rect')
+                        .attr('x', function(d, i) {return 0})
+                        .attr('y', function(d, i) {return i * height + (i + 1) * padding})
+                        .attr("width", width)
+                        .attr("height", height)
+        
+        
+        //var cx = this.options.width / 2;
+        //var cy = this.options.height / 2;
+        
+        this.__updateStyles(chart)
+
+        chart.transition()
+             .duration(this.options.transitionTime)
+             .attr('width', width)
+
+        //chart.exit().remove()
+    }
+};
+
+
 dynamic3.__ctors = {
-    'Circle': dynamic3.CircleGraph
+    'Circle': dynamic3.CircleGraph,
+    'Bar': dynamic3.BarGraph
 };
 
 dynamic3.createGraph = function(str) {
--- strawman.js
+++ strawman.js
@@ -1,12 +1,21 @@
 window.onload = function() {
     var graph = dynamic3.createGraph('Circle')
                         .setBackgroundColor('white')
-                        .setBorderColor('black')
+                        .setBorderColor('green')
                         .setBorderWidth('2px')
                         .setWidth(500)
                         .setHeight(500)
+                        .setDomain([0,100])
                         .setTransitionTime(400); // 200ms transition time from state to state.
+    
+    var bar = dynamic3.createGraph('Bar')
+                        .setWidth(500)
+                        .setHeight(600)
+                        .setDomain([0,100])
+                        .setPadding(2)
+                        .setTransitionTime(400);
 
+    bar.finishSetup(document.getElementById("target2"));
     graph.finishSetup(document.getElementById("target")); // This will connect our d3 graph with the dom element.
     // For now, it may be simpler to impose the rule that all styles must be decided before this finishSetup function is called.
     // Like the above setup would maybe throw an error after we've already "finished our setup". I'm not sure if this will
@@ -17,6 +26,11 @@ window.onload = function() {
     function loopWithRandomDynamicData() {
         var data = Math.random() * 100; // random number between 0 and 100
         graph.update(data); // This will be the heart of our API, something that just updates from one state to the next.
+        bardata = [];
+        for (var i = 0; i < 10; i += 1) {
+            bardata[i] = Math.random() * 100;
+        }
+        bar.update(bardata);
         var waitTime = Math.random() * 3000; // wait between 0 and 3 seconds for next data point.
         setTimeout(loopWithRandomDynamicData, waitTime)
     }
