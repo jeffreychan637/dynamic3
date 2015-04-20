7e5152e05c41b6637c4ad083108a1cbe450c5d99
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-04-19 21:51:26 -0700

    Fixed up tutorial by changing graph size, adding styles, etc.

--- website/scripts/tutorial.js
+++ website/scripts/tutorial.js
@@ -4,27 +4,29 @@ window.onload = function() {
 
 
     var bar = dynamic3.createGraph('BarGraph')
-                        .setWidth('300px')
-                        .setHeight('300px')
-                        .setDomain([0, 100])
-                        .setPadding(10)
-                        .setTransitionTime(400)
-                        .setBackgroundColor('#496dff')
-                        .setBorderColor('#2b2c2b')
-                        .setBorderWidth(1)
-                        .setOrientation('horizontal')
-                        .setText(getDataText)
+    
+    bar.setWidth('400px')
+       .setHeight('200px')
+       .setDomain([0, 100])
+       .setPadding(10)
+       .setTransitionTime(400)
+       .setBackgroundColor('#496dff')
+       .setBorderColor('#2b2c2b')
+       .setBorderWidth(1)
+       .setOrientation('horizontal')
+       .setText(getDataText);
 
     bar.insertIntoHTMLElement(document.getElementById("tutorial-bar-graph"));
     
     function loopWithRandomDynamicData() {
         var bardata = [];
-        for (var i = 0; i < 10; i += 1) {
-            bardata[i] = Math.random() * 100 | 0;
+        for (var i = 0; i < 5; i += 1) {
+            bardata[i] = Math.floor(Math.random() * 100) | 0;
         }
         bar.update(bardata);
         var waitTime = Math.random() * 3000; // wait between 0 and 3 seconds for next data point.
-        setTimeout(loopWithRandomDynamicData, waitTime) //In order to simulate the data coming in, we call this function over and over again after a random amount of seconds each time.
+        setTimeout(loopWithRandomDynamicData, waitTime)
+        //In order to simulate the data coming in, we call this function over and over again after a random amount of seconds each time.
 
     }
 
@@ -32,5 +34,5 @@ window.onload = function() {
 }
 
 function getDataText(data) {
-        return Math.floor(data);
+        return data;
 }
